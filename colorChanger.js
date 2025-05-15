3b13283d0e3674aee0d4b4e0e13819eb6438e38a
dereckc210 <dereck.pirulin@icloud.com>
2025-05-15 12:23:49 -0400

    Update colorChanger.js

--- colorChanger.js
+++ colorChanger.js
@@ -1,3 +1,6 @@
 let button = document.querySelector("colorButton");
 let box = document.querySelector("colorBox");
 
+button.addEventListener("click", function() {
+  box.style.backgroundColor = "blue";
+});
